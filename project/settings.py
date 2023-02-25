8d83347a67d86aeee8935fd62f0800ef3f4bacd6
Kirill <shkolenko02-03-2004@mail.ru>
2023-02-25 14:50:42 +0300

    Fixed naming

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'proejct.urls'
+ROOT_URLCONF = 'project.urls'
 
 TEMPLATES = [
     {
@@ -55,7 +55,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'proejct.wsgi.application'
+WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {}
 
